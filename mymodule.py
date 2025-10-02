ccae8c0e9b6ce79726a08fa9a37fbd52b04d0d16
balagp84 <balagp84@gmail.com>
2025-10-02 14:54:33 +0800

    Updated get_weather() function
    Updated get_weather() function to get_weather_student1().

--- mymodule.py
+++ mymodule.py
@@ -3,7 +3,7 @@ import json
 
 api_key = "xxxxxxx" 
 
-def get_weather(city): 
+def get_weather_student1(city): 
   sock = urlopen("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + api_key) 
   result = sock.read() 
   sock.close() 
